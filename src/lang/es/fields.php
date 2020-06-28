8d3e26c6d2fc9d675ce3595cb02c9b0a70f7c44a
Eduardo Mejia <edumejia30@gmail.com>
2020-06-28 12:00:43 -0400

    fix

--- src/lang/es/fields.php
+++ src/lang/es/fields.php
@@ -64,5 +64,7 @@ return [
     'product_bridge_variation_option' => 'Opción de Variación de Producto',
     'variation_option' => 'Opción de Variación',
     'product_bridge_channel' => 'Canales de Venta',
+    'channel' => 'Canal de Venta',
+    'subscription' => 'Suscripción',
 
 ];
